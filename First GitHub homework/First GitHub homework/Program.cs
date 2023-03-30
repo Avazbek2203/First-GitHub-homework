87bba90b62593dc30f1490ec3bf9b6296908d1f7
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-30 05:28:39 +0500

    Multiplication Table

--- First GitHub homework/First GitHub homework/Program.cs
+++ First GitHub homework/First GitHub homework/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace First_GitHub_homework
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int number = 0;
+        bool validInput = false;
+
+        while (!validInput)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out number))
+            {
+                validInput = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, please try again.");
+            }
+        }
+
+
+        for (int row = 1; row <= number; row++)
+        {
+
+            for (int coloumn = 1; coloumn <= number; coloumn++)
+            {
+
+                Console.Write("{0,4}", row * coloumn);
+            }
+            Console.WriteLine();
         }
     }
 }
