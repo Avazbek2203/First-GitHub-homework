10d4103456349516fdd1024b84603bf3b545ff00
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-30 05:26:13 +0500

    Perfect Number

--- First GitHub homework/First GitHub homework/Program.cs
+++ First GitHub homework/First GitHub homework/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace First_GitHub_homework
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int lowerBound = 0, upperBound = 0;
+        bool validLower = false, validUpper = false;
+
+        while (!validLower)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter the lower bound of the range: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out lowerBound))
+            {
+                validLower = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for lower bound, please try again.");
+            }
+        }
+
+        while (!validUpper)
+        {
+            Console.Write("Enter the upper bound of the range: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out upperBound))
+            {
+                validUpper = true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for upper bound, please try again.");
+            }
+        }
+
+        for (int number = lowerBound; number <= upperBound; number++)
+        {
+            int quantity = 0;
+
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    quantity += i;
+                }
+            }
+
+            if (quantity == number)
+            {
+                Console.WriteLine(number + " is a perfect number.");
+            }
         }
     }
 }
