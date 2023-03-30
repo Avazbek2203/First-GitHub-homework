e14fb69dee8a3d986eb463afeb2fef45f350592c
Avazbek2203 <128214329+Avazbek2203@users.noreply.github.com>
2023-03-30 05:36:52 +0500

    Convert to binary number

--- First GitHub homework/First GitHub homework/Program.cs
+++ First GitHub homework/First GitHub homework/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace First_GitHub_homework
+﻿using System;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write("Enter a decimal number: ");
+        int decimalNumber;
+        while (!int.TryParse(Console.ReadLine(), out decimalNumber))
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter only an integer number, please! ");
+            Console.Write("Enter a decimal number: ");
         }
+
+        string binaryNumber = Convert.ToString(decimalNumber, 2);
+        Console.WriteLine("Binary number: " + binaryNumber);
     }
 }
